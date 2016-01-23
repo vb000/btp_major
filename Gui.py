281bb757f178b048f4a5a53beeb820da5b1046a6
Prakash Ponduru <prakashpond@gmail.com>
2016-01-23 10:10:34 +0530

    ground lines included

--- Gui.py
+++ Gui.py
@@ -1,6 +1,6 @@
 from Tkinter import *
 import numpy as np
-import constants
+from constants import *
 import lineparams
         
 class Gui:
@@ -11,7 +11,7 @@ class Gui:
        self.line_type.set("Double line configuration")
     
        self.main_window = parent
-       self.main_window.geometry("1200x540")
+       self.main_window.geometry("1060x720")
      
        self.lineselection_frame = Frame(parent,relief=RIDGE,borderwidth=2,height=50)
        self.lineselection_frame.pack(side=TOP,expand=NO,fill=X)
@@ -37,7 +37,10 @@ class Gui:
            button = Radiobutton(self.lineselection_frame, text=str(option),font=12,value=option, 
                command=self.refresh_entries, variable=self.line_type).pack(side=LEFT,padx=5,pady=3)
        self.coordinate_frame=Frame(self.linegeometry_frame)
-       self.coordinate_frame.pack(side=TOP,expand=YES,fill=BOTH,padx=10,pady=10,ipadx=10,ipady=10)
+       self.coordinate_frame.pack(side=TOP,expand=YES,fill=BOTH,padx=5,pady=5,ipadx=5,ipady=5)
+       
+       self.groundline_frame=Frame(self.linegeometry_frame)
+       self.groundline_frame.pack(side=TOP,expand=YES,fill=BOTH,padx=3,pady=3,ipadx=2,ipady=2)
        
        self.entries={}
        self.conductorno_frame=Frame(self.coordinate_frame)
@@ -46,7 +49,15 @@ class Gui:
        self.x_frame.pack(side=LEFT,expand=NO,fill=Y)
        self.y_frame=Frame(self.coordinate_frame)
        self.y_frame.pack(side=LEFT,expand=NO,fill=Y)
-      
+
+       Label(self.groundline_frame, text="Number of ground conductors:",\
+             font=("Times",12,"italic"),padx=0,pady=0, justify=LEFT).pack(side=LEFT, anchor=W)
+       self.groundconductors_no=IntVar()
+       self.groundconductors_no.set(2)
+       for option in (0,1,2):
+           button = Radiobutton(self.groundline_frame, text=str(option),font=10,value=option,\
+           variable=self.groundconductors_no,command=self.refresh_groundno).pack(side=LEFT,padx=5,pady=3)
+
        self.entries[11]=Entry(self.conductorno_frame,width=12,justify=CENTER)
        self.entries[11].pack(side=TOP,anchor=N)
        self.entries[11].insert(0,"Conductor No")
@@ -62,13 +73,17 @@ class Gui:
        self.entries[13].insert(0,"Y-Coordinate(in M)")
        self.entries[13].configure(state='readonly')
       
-       for i in range(6):
+       for i in range(8):
            self.entries[(i+2)*10+1]=Entry(self.conductorno_frame,width=12,justify=CENTER,fg='red')
            self.entries[(i+2)*10+1].pack(side=TOP,anchor=N)
-           self.entries[(i+2)*10+1].insert(0,str(i+1))
+	   if i<=5:
+               self.entries[(i+2)*10+1].insert(0,str(i+1))
+	   else:
+	       self.entries[(i+2)*10+1].insert(0,str(i-5))
+
            self.entries[(i+2)*10+1].configure(state='readonly')
      
-       for i in range(6):
+       for i in range(8):
            self.entries[(i+2)*10+2]=Entry(self.x_frame,width=20,justify=CENTER)
            self.entries[(i+2)*10+2].pack(side=TOP,anchor=N)
        self.entries[22].insert(0,float(-11.0))
@@ -77,8 +92,10 @@ class Gui:
        self.entries[52].insert(0,float(-11.0))
        self.entries[62].insert(0,float(0.0))
        self.entries[72].insert(0,float(11.0))
+       self.entries[82].insert(0,float(-5.0))
+       self.entries[92].insert(0,float(5.0))
       
-       for i in range(6):
+       for i in range(8):
            self.entries[(i+2)*10+3]=Entry(self.y_frame,width=20,justify=CENTER)
            self.entries[(i+2)*10+3].pack(side=TOP,anchor=N)
        self.entries[23].insert(0,float(15.0))
@@ -87,10 +104,18 @@ class Gui:
        self.entries[53].insert(0,float(10.0))
        self.entries[63].insert(0,float(10.0))
        self.entries[73].insert(0,float(10.0))
+       self.entries[83].insert(0,float(20.0))
+       self.entries[93].insert(0,float(20.0))
+
+       self.phaseconductor_frame=Frame(self.linegeometry_frame)
+       self.phaseconductor_frame.pack(side=TOP,fill=BOTH,expand=YES)
+      
+       Label(self.phaseconductor_frame, text="Phase conductor and bundle parameters",\
+             font=("Helvetica",12,"bold"),fg="blue",padx=5,pady=5, justify=LEFT).pack(side=TOP, anchor=W)
       
        self.bundle_frame=Frame(self.linegeometry_frame,relief=SUNKEN,borderwidth=2)
-       self.bundle_frame.pack(side=TOP,fill=BOTH,expand=YES)
-      
+       self.bundle_frame.pack(side=TOP,fill=BOTH,expand=YES,padx=5,pady=5,ipadx=5,ipady=5)
+     
       
        self.label_frame=Frame(self.bundle_frame)
        self.label_frame.pack(side=LEFT,expand=YES,fill=BOTH)
@@ -129,9 +154,61 @@ class Gui:
        self.entries[5]=Entry(self.entry_frame,width=12,justify=CENTER)
        self.entries[5].pack(side=TOP,anchor=W)
        self.entries[5].insert(0,float(2.0))
+       
+       self.groundconductor_frame=Frame(self.linegeometry_frame)
+       self.groundconductor_frame.pack(side=TOP,fill=BOTH,expand=YES)
+      
+       Label(self.groundconductor_frame, text="Ground conductor and bundle parameters",\
+             font=("Helvetica",12,"bold"),padx=5,pady=5,fg="blue", justify=LEFT).pack(side=TOP, anchor=W)
+ 
+       self.bundleg_frame=Frame(self.linegeometry_frame,relief=SUNKEN,borderwidth=2)
+       self.bundleg_frame.pack(side=TOP,fill=BOTH,expand=YES,padx=5,pady=5,ipadx=10,ipady=10)
+     
+      
+       self.labelg_frame=Frame(self.bundleg_frame)
+       self.labelg_frame.pack(side=LEFT,expand=YES,fill=BOTH)
+     
+       self.entryg_frame=Frame(self.bundleg_frame,padx=3,pady=3)
+       self.entryg_frame.pack(side=TOP,expand=YES,fill=BOTH)
+
+      
+       Label(self.labelg_frame, text="Radius of the conductor(in M):",\
+             font=("Times",11,"bold"),padx=0,pady=0, justify=LEFT).pack(side=TOP, anchor=W)
+       self.entries[6]=Entry(self.entryg_frame,width=12,justify=CENTER)
+       self.entries[6].pack(side=TOP,anchor=W)
+       self.entries[6].insert(0,float(0.004725))
+
+       Label(self.labelg_frame, text="Bundle Radius(in M):",\
+            font=("Times",11,"bold"),padx=0,pady=0, justify=LEFT).pack(side=TOP, anchor=W)
+       self.entries[7]=Entry(self.entryg_frame,width=12,justify=CENTER)
+       self.entries[7].pack(side=TOP,anchor=W)
+       self.entries[7].insert(0,float(.288))
+
+
+       Label(self.labelg_frame, text="Effective Radius(in M):",\
+            font=("Times",11,"bold"),padx=0,pady=0, justify=LEFT).pack(side=TOP, anchor=W)
+       self.entries[8]=Entry(self.entryg_frame,width=12,justify=CENTER)
+       self.entries[8].pack(side=TOP,anchor=W)
+       self.entries[8].insert(0,float(0.004725))
+
+       Label(self.labelg_frame, text="DC Resistance of the conductor(in M):",\
+            font=("Times",11,"bold"),padx=0,pady=0, justify=LEFT).pack(side=TOP, anchor=W)
+       self.entries[9]=Entry(self.entryg_frame,width=12,justify=CENTER)
+       self.entries[9].pack(side=TOP,anchor=W)
+       self.entries[9].insert(0,float(0.035))
+    
+       Label(self.labelg_frame, text="Number of conductors in the bundle:",
+            font=("Times",11,"bold"),padx=0,pady=0, justify=LEFT).pack(side=TOP, anchor=W)
+       self.entries[10]=Entry(self.entryg_frame,width=12,justify=CENTER)
+       self.entries[10].pack(side=TOP,anchor=W)
+       self.entries[10].insert(0,float(1.0))
 
        self.computeRLC_button=Button(self.output_frame,text="Compute RLC parameters",command=self.compute)
        self.computeRLC_button.pack(side=LEFT)
+
+       self.voltagegradient_button=Button(self.output_frame,text="Compute Surface Voltage Gradient",command=self.compute)
+       self.voltagegradient_button.pack(side=LEFT,padx=5)
+
        
        self.text=Text(self.result_frame)
        self.text.pack(expand=TRUE, fill='both')
@@ -139,21 +216,72 @@ class Gui:
 
    def compute(self):
        obj = None
-       if self.line_type.get()=="Single line configuration":
-           obj = lineparams.Single_circuit(a1=(float(self.entries[22].get()),float(self.entries[23].get()))\
-                                          ,a2=(float(self.entries[32].get()),float(self.entries[33].get()))\
-                                          ,a3=(float(self.entries[42].get()),float(self.entries[43].get()))\
-                                          ,N=float(self.entries[5].get())\
-                                          ,Rb=float(self.entries[2].get()))
+       cp=ACSR(float(self.entries[4].get()),float(self.entries[1].get()),float(self.entries[3].get()))
+       cg=ACSR(float(self.entries[9].get()),float(self.entries[6].get()),float(self.entries[8].get()))
+       if self.groundconductors_no.get()==0:
+           if self.line_type.get()=="Single line configuration":
+               obj = lineparams.Single_circuit(a1=(float(self.entries[22].get()),float(self.entries[23].get()))\
+                                              ,a2=(float(self.entries[32].get()),float(self.entries[33].get()))\
+                                              ,a3=(float(self.entries[42].get()),float(self.entries[43].get()))\
+                                              ,Np=float(self.entries[5].get())\
+                                              ,Rbp=float(self.entries[2].get()),conductor_p=cp)
+           else:
+               obj = lineparams.Double_circuit(a1=(float(self.entries[22].get()),float(self.entries[23].get()))\
+                                              ,a2=(float(self.entries[32].get()),float(self.entries[33].get()))\
+                                              ,a3=(float(self.entries[42].get()),float(self.entries[43].get()))\
+                                              ,a4=(float(self.entries[52].get()),float(self.entries[53].get()))\
+                                              ,a5=(float(self.entries[62].get()),float(self.entries[63].get()))\
+                                              ,a6=(float(self.entries[72].get()),float(self.entries[73].get()))\
+                                              ,Np=float(self.entries[5].get())\
+                                              ,Rbp=float(self.entries[2].get()),conductor_p=cp)
+       elif self.groundconductors_no.get()==1:
+           if self.line_type.get()=="Single line configuration":
+               obj = lineparams.Single_circuit_1g(a1=(float(self.entries[22].get()),float(self.entries[23].get()))\
+                                                  ,a2=(float(self.entries[32].get()),float(self.entries[33].get()))\
+                                                  ,a3=(float(self.entries[42].get()),float(self.entries[43].get()))\
+                                                  ,a4=(float(self.entries[82].get()),float(self.entries[83].get()))\
+                                                  ,Np=float(self.entries[5].get())\
+                                                  ,Ng=float(self.entries[10].get())\
+                                                  ,Rbp=float(self.entries[2].get())\
+                                                  ,Rbg=float(self.entries[7].get()),conductor_p=cp,conductor_g=cg)
+           else:
+               obj = lineparams.Double_circuit_1g(a1=(float(self.entries[22].get()),float(self.entries[23].get()))\
+                                                 ,a2=(float(self.entries[32].get()),float(self.entries[33].get()))\
+                                                 ,a3=(float(self.entries[42].get()),float(self.entries[43].get()))\
+                                                 ,a4=(float(self.entries[52].get()),float(self.entries[53].get()))\
+                                                 ,a5=(float(self.entries[62].get()),float(self.entries[63].get()))\
+                                                 ,a6=(float(self.entries[72].get()),float(self.entries[73].get()))\
+                                                 ,a7=(float(self.entries[82].get()),float(self.entries[83].get()))\
+                                                 ,Np=float(self.entries[5].get())\
+                                                 ,Ng=float(self.entries[10].get())\
+                                                 ,Rbp=float(self.entries[2].get())\
+                                                 ,Rbg=float(self.entries[7].get()),conductor_p=cp,conductor_g=cg)
+
        else:
-           obj = lineparams.Double_circuit(a1=(float(self.entries[22].get()),float(self.entries[23].get()))\
-                                          ,a2=(float(self.entries[32].get()),float(self.entries[33].get()))\
-                                          ,a3=(float(self.entries[42].get()),float(self.entries[43].get()))\
-                                          ,a4=(float(self.entries[52].get()),float(self.entries[53].get()))\
-                                          ,a5=(float(self.entries[62].get()),float(self.entries[63].get()))\
-                                          ,a6=(float(self.entries[72].get()),float(self.entries[73].get()))\
-                                          ,N=float(self.entries[5].get())\
-                                          ,Rb=float(self.entries[2].get()))
+           if self.line_type.get()=="Single line configuration":
+               obj = lineparams.Single_circuit_2g(a1=(float(self.entries[22].get()),float(self.entries[23].get()))\
+                                                  ,a2=(float(self.entries[32].get()),float(self.entries[33].get()))\
+                                                  ,a3=(float(self.entries[42].get()),float(self.entries[43].get()))\
+                                                  ,a4=(float(self.entries[82].get()),float(self.entries[83].get()))\
+                                                  ,a5=(float(self.entries[92].get()),float(self.entries[93].get()))\
+                                                  ,Np=float(self.entries[5].get())\
+                                                  ,Ng=float(self.entries[10].get())\
+                                                  ,Rbp=float(self.entries[2].get())\
+                                                  ,Rbg=float(self.entries[7].get()),conductor_p=cp,conductor_g=cg)
+           else:
+               obj = lineparams.Double_circuit_2g(a1=(float(self.entries[22].get()),float(self.entries[23].get()))\
+                                                 ,a2=(float(self.entries[32].get()),float(self.entries[33].get()))\
+                                                 ,a3=(float(self.entries[42].get()),float(self.entries[43].get()))\
+                                                 ,a4=(float(self.entries[52].get()),float(self.entries[53].get()))\
+                                                 ,a5=(float(self.entries[62].get()),float(self.entries[63].get()))\
+                                                 ,a6=(float(self.entries[72].get()),float(self.entries[73].get()))\
+                                                 ,a7=(float(self.entries[82].get()),float(self.entries[83].get()))\
+                                                 ,a8=(float(self.entries[92].get()),float(self.entries[93].get()))\
+                                                 ,Np=float(self.entries[5].get())\
+                                                 ,Ng=float(self.entries[10].get())\
+                                                 ,Rbp=float(self.entries[2].get())\
+                                                 ,Rbg=float(self.entries[7].get()),conductor_p=cp,conductor_g=cg)
+
        np.set_printoptions(precision=3)
        Lout = "L=\n" + str(obj.L()) + " mH/km\n"
        Cout = "C=\n" + str(obj.C()) + " nF/km\n"
@@ -180,10 +308,27 @@ class Gui:
           self.entries[53]["state"]=NORMAL
           self.entries[63]["state"]=NORMAL
           self.entries[73]["state"]=NORMAL
+   
+   def refresh_groundno(self):
+       if self.groundconductors_no.get()==0:
+           self.entries[82]["state"]=DISABLED
+	   self.entries[92]["state"]=DISABLED
+	   self.entries[83]["state"]=DISABLED
+	   self.entries[93]["state"]=DISABLED
+       elif self.groundconductors_no.get()==1:
+           self.entries[82]["state"]=NORMAL
+	   self.entries[83]["state"]=NORMAL
+	   self.entries[92]["state"]=DISABLED
+	   self.entries[93]["state"]=DISABLED
+       else:
+           self.entries[82]["state"]=NORMAL
+	   self.entries[92]["state"]=NORMAL
+           self.entries[83]["state"]=NORMAL
+	   self.entries[93]["state"]=NORMAL
 
         
 if __name__ == "__main__":
     root = Tk()
     root.title("EHV AC Transmission Line GUI")
     gui  = Gui(root)
-    root.mainloop()
+root.mainloop()
--- lineparams.py
+++ lineparams.py
@@ -2,14 +2,15 @@ import numpy as np
 from constants import *
 
 class Single_circuit:
-   def __init__(self,a1,a2,a3,N,Rb):
+   def __init__(self,a1,a2,a3,Np,Rbp,conductor_p):
        self.a1 = a1
        self.a2 = a2
        self.a3 = a3
-       self.N  = N
-       self.Rb = Rb
+       self.Np = Np
+       self.Rbp = Rbp
+       self.conductor_p=conductor_p
    def P(self):
-      gmr = self.Rb*((self.N*conductor.Reff/self.Rb)**(1.0/self.N))
+      gmr = self.Rbp*((self.Np*self.conductor_p.Reff/self.Rbp)**(1.0/self.Np))
       P_mat = np.zeros((5,5))
     
       P_mat[1,1] = np.log(2*self.a1[1]/gmr)
@@ -37,18 +38,121 @@ class Single_circuit:
       Linv = np.linalg.inv(self.L())
       return (Linv/(g**2))*1e12 # nF/km
 
+class Single_circuit_1g:
+   def __init__(self,a1,a2,a3,a4,Np,Ng,Rbp,Rbg,conductor_p,conductor_g):
+       self.a1 = a1
+       self.a2 = a2
+       self.a3 = a3
+       self.a4 = a4
+       self.Np = Np
+       self.Ng = Ng
+       self.Rbp = Rbp
+       self.Rbg = Rbg
+       self.conductor_p=conductor_p
+       self.conductor_g=conductor_g
+   def P(self):
+      gmr_p = self.Rbp*((self.Np*self.conductor_p.Reff/self.Rbp)**(1.0/self.Np))
+      gmr_g = self.Rbg*((self.Ng*self.conductor_g.Reff/self.Rbg)**(1.0/self.Ng))
+      
+      x   = [0,self.a1[0],self.a2[0],self.a3[0],self.a4[0]] # x-coordinates
+      y   = [0,self.a1[1],self.a2[1],self.a3[1],self.a4[1]] # y-coordinates
+    
+      P_mat = np.zeros((5,5))
+    
+      for i in range(1,4):
+          P_mat[i,i] = np.log(2*y[i]/gmr_p)
+
+      P_mat[4,4] = np.log(2*y[4]/gmr_g)
+    
+      for i in range(1,5):
+          for j in range(i+1,5):
+              P_mat[i,j] = np.log( np.sqrt(((x[i]-x[j])**2.0) + ((y[i]+y[j])**2.0))\
+                                     /np.sqrt(((x[i]-x[j])**2.0) + ((y[i]-y[j])**2.0))\
+                                   ) # ln(I12/A12)
+              P_mat[j,i]=P_mat[i,j]
+    
+        
+      p = P_mat[1:4,1:4]#phase p
+      u = P_mat[4:5,1:4]
+      v = P_mat[4:5,4:5]
+      w = P_mat[1:4,4:5]
+      vinv= np.linalg.inv(v)
+      p_r =reduce(np.dot ,[w,vinv,u])
+      p_red = p-p_r
+      return p_red
+
+
+   def L(self):
+      return 0.2 * self.P() # mH/km
+
+   def C(self):
+      Linv = np.linalg.inv(self.L())
+      return (Linv/(g**2))*1e12 # nF/km
+
+class Single_circuit_2g:
+   def __init__(self,a1,a2,a3,a4,a5,Np,Ng,Rbp,Rbg,conductor_p,conductor_g):
+       self.a1 = a1
+       self.a2 = a2
+       self.a3 = a3
+       self.a4 = a4
+       self.a5 = a5
+       self.Np = Np
+       self.Ng = Ng
+       self.Rbp = Rbp
+       self.Rbg = Rbg
+       self.conductor_p=conductor_p
+       self.conductor_g=conductor_g
+   def P(self):
+      gmr_p = self.Rbp*((self.Np*self.conductor_p.Reff/self.Rbp)**(1.0/self.Np))
+      gmr_g = self.Rbg*((self.Ng*self.conductor_g.Reff/self.Rbg)**(1.0/self.Ng))
+      x   = [0,self.a1[0],self.a2[0],self.a3[0],self.a4[0],self.a5[0]] # x-coordinates
+      y   = [0,self.a1[1],self.a2[1],self.a3[1],self.a4[1],self.a5[1]] # y-coordinates
+    
+      P_mat = np.zeros((6,6))
+    
+      for i in range(1,4):
+          P_mat[i,i] = np.log(2*y[i]/gmr_p)
+    
+      for i in range(4,6):
+          P_mat[i,i] = np.log(2*y[i]/gmr_g)
+      
+      for i in range(1,6):
+          for j in range(i+1,6):
+              P_mat[i,j] = np.log( np.sqrt(((x[i]-x[j])**2.0) + ((y[i]+y[j])**2.0))\
+                                     /np.sqrt(((x[i]-x[j])**2.0) + ((y[i]-y[j])**2.0))\
+                                   ) # ln(I12/A12)
+              P_mat[j,i]=P_mat[i,j]
+    
+        
+      p = P_mat[1:4,1:4]#phase p
+      u = P_mat[4:6,1:4]
+      v = P_mat[4:6,4:6]
+      w = P_mat[1:4,4:6]
+      vinv= np.linalg.inv(v)
+      p_r =reduce(np.dot ,[w,vinv,u])
+      p_red = p-p_r
+      return p_red
+
+   def L(self):
+      return 0.2 * self.P() # mH/km
+
+   def C(self):
+      Linv = np.linalg.inv(self.L())
+      return (Linv/(g**2))*1e12 # nF/km
+
 class Double_circuit:
-   def __init__(self,a1,a2,a3,a4,a5,a6,N,Rb):
+   def __init__(self,a1,a2,a3,a4,a5,a6,Np,Rbp,conductor_p):
         self.a1 = a1
         self.a2 = a2
         self.a3 = a3
         self.a4 = a4
         self.a5 = a5
         self.a6 = a6
-        self.N  = N
-        self.Rb = Rb
+        self.Np  = Np
+        self.Rbp = Rbp
+        self.conductor_p=conductor_p
    def P(self):
-        gmr = self.Rb*((self.N*conductor.Reff/self.Rb)**(1.0/self.N))
+        gmr = self.Rbp*((self.Np*self.conductor_p.Reff/self.Rbp)**(1.0/self.Np))
         x   = [0,self.a1[0],self.a2[0],self.a3[0],self.a4[0],self.a5[0],self.a6[0]] # x-coordinates
         y   = [0,self.a1[1],self.a2[1],self.a3[1],self.a4[1],self.a5[1],self.a6[1]] # y-coordinates
     
@@ -73,9 +177,116 @@ class Double_circuit:
         Linv = np.linalg.inv(self.L())
         return (Linv/(g**2))*1e12 # nF/km
 
-if __name__=="__main__":
-   line1 = Single_circuit((-11.0,15.0), (0.0,15.0), (11.0,15.0), 2, .2288)
-   line2 = Double_circuit((-11.0,15.0), (0.0,15.0), (11.0,15.0), (-11.0,10.0), (0.0,10.0), (11.0,10.0), 2, .2288)
-   print(line1.C())
-   print(line2.C())
+class Double_circuit_1g:
+   def __init__(self,a1,a2,a3,a4,a5,a6,a7,Np,Ng,Rbp,Rbg,conductor_p,conductor_g):
+        self.a1 = a1
+        self.a2 = a2
+        self.a3 = a3
+        self.a4 = a4
+        self.a5 = a5
+        self.a6 = a6
+        self.a7 = a7
+        self.Np = Np
+        self.Ng = Ng
+        self.Rbp = Rbp
+        self.Rbg = Rbg
+        self.conductor_p=conductor_p
+        self.conductor_g=conductor_g
+   def P(self):
+        gmr_p = self.Rbp*((self.Np*self.conductor_p.Reff/self.Rbp)**(1.0/self.Np))
+        gmr_g = self.Rbg*((self.Ng*self.conductor_g.Reff/self.Rbg)**(1.0/self.Ng))
+        x   = [0,self.a1[0],self.a2[0],self.a3[0],self.a4[0],self.a5[0],self.a6[0],self.a7[0]] # x-coordinates
+        y   = [0,self.a1[1],self.a2[1],self.a3[1],self.a4[1],self.a5[1],self.a6[1],self.a7[1]] # y-coordinates
+    
+        P_mat = np.zeros((8,8))
+    
+        for i in range(1,7):
+            P_mat[i,i] = np.log(2*y[i]/gmr_p)
+        P_mat[7,7] = np.log(2*y[7]/gmr_g)
+   
+        for i in range(1,8):
+            for j in range(i+1,8):
+                P_mat[i,j] = np.log( np.sqrt(((x[i]-x[j])**2.0) + ((y[i]+y[j])**2.0))\
+                                     /np.sqrt(((x[i]-x[j])**2.0) + ((y[i]-y[j])**2.0))\
+                                   ) # ln(I12/A12)
+                P_mat[j,i]=P_mat[i,j]
+    
+        
+        p = P_mat[1:7,1:7]#phase p
+        u = P_mat[7:8,1:7]
+        v = P_mat[7:8,7:8]
+        w = P_mat[1:7,7:8]
+        vinv= np.linalg.inv(v)
+        p_r =reduce(np.dot ,[w,vinv,u])
+        p_red = p-p_r
+        return p_red
 
+   def L(self):
+        return 0.2*self.P() # mH/km
+
+   def C(self):
+        Linv = np.linalg.inv(self.L())
+        return (Linv/(g**2))*1e12 # nF/km
+
+class Double_circuit_2g:
+   def __init__(self,a1,a2,a3,a4,a5,a6,a7,a8,Np,Ng,Rbp,Rbg,conductor_p,conductor_g):
+        self.a1 = a1
+        self.a2 = a2
+        self.a3 = a3
+        self.a4 = a4
+        self.a5 = a5
+        self.a6 = a6
+        self.a7 = a7
+        self.a8 = a8
+        self.Np = Np
+        self.Ng = Ng
+        self.Rbp = Rbp
+        self.Rbg = Rbg
+        self.conductor_p=conductor_p
+        self.conductor_g=conductor_g
+   def P(self):
+        gmr_p = self.Rbp*((self.Np*self.conductor_p.Reff/self.Rbp)**(1.0/self.Np))
+        gmr_g = self.Rbg*((self.Ng*self.conductor_g.Reff/self.Rbg)**(1.0/self.Ng))
+        
+        x   = [0,self.a1[0],self.a2[0],self.a3[0],self.a4[0],self.a5[0],self.a6[0],self.a7[0],self.a8[0]] # x-coordinates
+        y   = [0,self.a1[1],self.a2[1],self.a3[1],self.a4[1],self.a5[1],self.a6[1],self.a7[1],self.a8[1]] # y-coordinates
+    
+        P_mat = np.zeros((9,9))
+    
+        for i in range(1,7):
+            P_mat[i,i] = np.log(2*y[i]/gmr_p)
+    
+        for i in range(7,9):
+            P_mat[i,i] = np.log(2*y[i]/gmr_g)
+        
+        for i in range(1,9):
+            for j in range(i+1,9):
+                P_mat[i,j] = np.log( np.sqrt(((x[i]-x[j])**2.0) + ((y[i]+y[j])**2.0))\
+                                     /np.sqrt(((x[i]-x[j])**2.0) + ((y[i]-y[j])**2.0))\
+                                   ) # ln(I12/A12)
+                P_mat[j,i]=P_mat[i,j]
+    
+        p = P_mat[1:7,1:7]#phase p
+        u = P_mat[7:9,1:7]
+        v = P_mat[7:9,7:9]
+        w = P_mat[1:7,7:9]
+        vinv= np.linalg.inv(v)
+        p_r =reduce(np.dot ,[w,vinv,u])
+        p_red = p-p_r
+        return p_red
+        
+
+   def L(self):
+        return 0.2*self.P() # mH/km
+
+   def C(self):
+        Linv = np.linalg.inv(self.L())
+        return (Linv/(g**2))*1e12 # nF/km
+
+if __name__=="__main__":
+   conductor_p=ACSR(Reff=0.01585)
+   conductor_g=ACSR(Reff=0.004725)
+   line1 = Single_circuit((-11.0,15.0), (0.0,15.0), (11.0,15.0), 2, .288,conductor_p)
+   line2 = Single_circuit_2g((-11.3,9.81), (0.0,9.81), (11.3,9.81),(-8.0,20.875), (8.0,20.875),2,1, .2288,0.004725,conductor_p,conductor_g)
+   print(line1.P())
+   print(line2.P())
